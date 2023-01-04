9780d59666b69cbf3a59c97d11af7a7cd0d67c91
dialite <raymondolisa01@gmail.com>
2023-01-04 16:23:12 +0100

    Assign object to a variable before exporting as module default IN APIServices.js

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,8 +1,8 @@
-import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { exchangeLists } from '../redux/exchange/exchange';
-import HomepageItem from './HomepageItem';
-import './Homepage.css';
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { exchangeLists } from "../redux/exchange/exchange";
+import HomepageItem from "./HomepageItem";
+import "./Homepage.css";
 
 const Homepage = () => {
   const dispatch = useDispatch();
@@ -11,17 +11,14 @@ const Homepage = () => {
     if (exchange.length === 0) {
       dispatch(exchangeLists());
     }
-  }, []);
+  }, [dispatch, exchange.length]);
 
   return (
     <div className="container">
       <ul className="exchange-list-conatiner">
         <h1 className="homepage-heading">Please Select Exchange</h1>
         {exchange.map((exchange) => (
-          <HomepageItem
-            key={exchange.symbol}
-            exchange={exchange}
-          />
+          <HomepageItem key={exchange.symbol} exchange={exchange} />
         ))}
       </ul>
     </div>
--- src/redux/APIServices.js
+++ src/redux/APIServices.js
@@ -1,8 +1,8 @@
-const API = 'https://financialmodelingprep.com/api/v3/';
+const API = "https://financialmodelingprep.com/api/v3/";
 
-const APIQUOTES = 'https://financialmodelingprep.com/api/v3/quote/';
+const APIQUOTES = "https://financialmodelingprep.com/api/v3/quote/";
 
-const KEY = 'b910dda5a91116817f2abf37854a3177';
+const KEY = "b910dda5a91116817f2abf37854a3177";
 // const KEY = '177e7470a52d9fcb6780331b067db588';
 // const KEY = 'c72f83b1dfc59f79c4cdee3c9b93c7f4';
 
@@ -10,9 +10,9 @@ const getCompanyData = async (symbol) => {
   const companyAPI = `${API}${symbol}_constituent?apikey=${KEY}`;
 
   const response = await fetch(companyAPI, {
-    method: 'GET',
+    method: "GET",
     headers: {
-      Accept: 'application/json',
+      Accept: "application/json",
     },
   });
   const companySymbolData = await response.json();
@@ -22,15 +22,23 @@ const getCompanyData = async (symbol) => {
 const getCompanyQuotes = async (exchangeName, symbol) => {
   const companyQuotesAPI = `${APIQUOTES}${symbol}?apikey=${KEY}`;
   const response = await fetch(companyQuotesAPI, {
-    method: 'GET',
+    method: "GET",
     headers: {
-      Accept: 'application/json',
+      Accept: "application/json",
     },
   });
   const companySymbolData = await response.json();
   const exchange = exchangeName.toUpperCase();
-  const companyDataWithExchange = companySymbolData.map((item) => ({ ...item, exchange }));
+  const companyDataWithExchange = companySymbolData.map((item) => ({
+    ...item,
+    exchange,
+  }));
   return companyDataWithExchange;
 };
 
-export default { getCompanyData, getCompanyQuotes };
+const companyInfo = {
+  getCompanyData,
+  getCompanyQuotes,
+};
+
+export default companyInfo;
